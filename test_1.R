cc78b9f2fb9fe2310bb2a9e48d4b42699d042d6a
charapink <charapinkk@qq.com>
2022-07-27 14:51:19 +0800

    Initial commit

---
+++ test_1.R
@@ -0,0 +1,15 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: ASUS
+# Created on: 2022/7/27
+
+library(ggplot2)
+rm(list = ls())
+dat <- data.frame(
+  time = factor(c("Lunch","Dinner"),levels = c("Lunch","Dinner")),
+  total_bill = c(14.89,17.23)
+
+)
+dat
+ggplot(data=dat,aes(x=time,y=total_bill,fill=time))+
+    geom_bar(stat = "identity")
